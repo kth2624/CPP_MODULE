1e235009308a335dec1b5027498e5eed2f72df8e
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-02-05 02:40:29 +0900

    CPP01/ex00, ex01, ex02 OK

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,19 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() : name("")
+{
+}
+
+Zombie::Zombie(std::string name) : name(name)
+{
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->name << " 무덤속으로..." << std::endl;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ CPP01/ex00/newZombie.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+Zombie *newZombie( std::string name )
+{
+	Zombie *newZom = new Zombie(name);
+	return (newZom);
+}
