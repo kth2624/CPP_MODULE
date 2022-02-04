0b20e557717c9855b5ef4ac6ce1153ad96838a11
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-02-04 19:26:09 +0900

    CPP00/ex01/main.cpp 수정

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -9,29 +9,22 @@ void suppot(void)
 	std::cout <<"------2.ADD----------------------------------" << std::endl;
 	std::cout <<"------3.SEARCH-------------------------------" << std::endl;
 	std::cout <<"---------------------------------------------" << std::endl;
-
 }
 
 int main(void)
 {
 	std::string input;
-	int num;
 	Phonebook book;
 
-	num = 0;
 	while(1)
 	{
 		suppot();
 		std::getline(std::cin, input);
 		if(input == "1" || input == "EXIT")
-		{
 			return (0);
-		}
 		else if(input == "2" || input == "ADD")
-		{
 			book.add();
-
-		}else if(input == "3" || input == "SEARCH")
+		else if(input == "3" || input == "SEARCH")
 		{
 			book.print_info();
 			std::cout << "show ? index ? -> input num!!" << std::endl;
@@ -42,7 +35,6 @@ int main(void)
 			std::cout << "|     index|first name| last name| nick name|"<<std::endl;
 			std::cout << "---------------------------------------------"<<std::endl;
 			book.oneline(atoi(input.c_str())-1);
-
 		}
 		std::cout << std::endl << std::endl;
 	}
