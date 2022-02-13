d807560a2cc13144d54034e7f5dc05bee108d213
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-02-13 23:03:00 +0900

    CPP04/ex02 추상클래스

---
+++ CPP04/ex02/Animal.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkim <tkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/12 04:13:22 by tkim              #+#    #+#             */
+/*   Updated: 2022/02/13 22:58:34 by tkim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal():type("Animal")
+{
+	std::cout << "Animal Default constructor called " << std::endl;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal Destructor called " << std::endl;
+}
+
+Animal::Animal(const Animal& ani)
+{
+	*this = ani;
+	std::cout << "Animal copy constructor called " << std::endl;
+}
+
+Animal& Animal::operator=(const Animal &ani)
+{
+	this->type = ani.getType();
+	return *this;
+}
+
+std::string Animal::getType()const
+{
+	return this->type;
+}
