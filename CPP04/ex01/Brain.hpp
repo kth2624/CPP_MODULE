0f03c3c06a9fcb74d37510e6d8eadbdf38ad4b06
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-02-14 01:13:27 +0900

    CPP04/ex01 업캐스팅,다운캐스팅

--- CPP04/ex01/Brain.hpp
+++ CPP04/ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: tkim <tkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 16:30:27 by tkim              #+#    #+#             */
-/*   Updated: 2022/02/13 17:09:57 by tkim             ###   ########.fr       */
+/*   Updated: 2022/02/14 01:07:17 by tkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define BRAIN_HPP
 #include <iostream>
 #include <string>
-
+//핵심
+//up casting 상속관계에서 하위클래스가 상위클래스로 형변환하는것
+//down casting 상속관계에서 업캐스팅했던 상위 클래스 인스턴스가 하위클래스로 형변환 하는것
 class Brain
 {
 	private:
