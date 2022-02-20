e68c7022a57d5f6ec7acc057c2bd911d6fdc1461
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-02-21 01:05:23 +0900

    CPP07/ex02 OK 클래스 템플릿

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,13 +6,12 @@
 /*   By: tkim <tkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 21:45:37 by tkim              #+#    #+#             */
-/*   Updated: 2022/02/20 21:46:46 by tkim             ###   ########.fr       */
+/*   Updated: 2022/02/21 00:57:58 by tkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-#include <iostream>
 
 /*
 template<typename T>
@@ -28,23 +27,81 @@ int main(void) {
   Test<std::stding> t_string("hello"); // 템플릿 인수 명시
 }*/
 
+template <typename T>
 class Array
 { 
 	private:
+		T *arr;
+		unsigned int n;
 	public:
 		Array();
-		Array(const Array& obj);
+		Array(unsigned int n);
+		Array(const Array<T>& obj);
 		~Array();
-		Array& operator=(const Array& obj);
+		Array& operator=(const Array<T>& obj);
+		T& operator[](unsigned int n);
+		const T& operator[](unsigned int n)const;
+		unsigned int size()const;
 };
 
-#endif
 
-Array::Array()
-{ }
-Array::Array(const Array& obj)
-{ }
-Array::~Array()
-{ }
-Array& Array::operator=(const Array& obj)
-{ }
+template<typename T>
+Array<T>::Array():arr(new T[0]),n(0)
+{
+
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n):arr(new T[n]),n(n)
+{
+
+}
+
+template<typename T>
+Array<T>::Array(const Array<T>& obj):arr(new T[obj.size()]),n(obj.size())
+{
+	*this = obj;
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	delete[] arr;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array<T>& obj)
+{
+	delete[] arr;
+	n = obj.size();
+	arr = new T[n];
+	for(unsigned int i = 0; i < n; i++)
+	{
+		arr[i] = obj.arr[i];
+	}
+	return *this;
+}
+
+template<typename T>
+T& Array<T>::operator[](unsigned int idx)
+{
+	if(n <= idx)
+		throw std::exception();
+	return arr[idx];
+}
+
+template<typename T>
+const T& Array<T>::operator[](unsigned int idx)const
+{
+	if(n <= idx)
+		throw std::exception();
+	return arr[idx];
+}
+
+template<typename T>
+unsigned int Array<T>::size()const
+{
+	return n;
+}
+
+#endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,15 +6,18 @@
 /*   By: tkim <tkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 20:33:10 by tkim              #+#    #+#             */
-/*   Updated: 2022/02/20 21:46:55 by tkim             ###   ########.fr       */
+/*   Updated: 2022/02/21 01:01:54 by tkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <Array.hpp>
+
+#include <iostream>
+#include "Array.hpp"
 
 #define MAX_VAL 750
 int main(int, char**)
 {
+    {
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
@@ -60,5 +63,77 @@ int main(int, char**)
         numbers[i] = rand();
     }
     delete [] mirror;//
+    }
+
+    {
+        Array<int> defaultArray = Array<int>();
+	std::cout << "\n\n\033[36m--- Create defaultArray by Array<int>() ---\033[0m\n\n";
+
+	std::cout << "defaultArray.size() : " <<  defaultArray.size() << std::endl;
+	try
+	{
+		std::cout << "defaultArray[0] : " << defaultArray[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "\033[31m" << e.what() << "\033[33m(OutOfBound)\033[0m\n";
+	}
+
+	Array<int> num = Array<int>(5);
+	std::cout << "\n\n\033[36m--- Create num by Array<int>(5) ---\033[0m\n\n";
+
+	std::cout << "num.size() : " << num.size() << std::endl;
+	try
+	{
+		std::cout << "num[0] : " << num[0] << std::endl;
+		num[0] = 6;
+        num[1] = 3;
+		std::cout << "\033[32m>> change num[0] value\033[0m\n";
+		std::cout << "\033[32m>> change num[1] value\033[0m\n";
+		std::cout <<  "num[0] : " << num[0] << std::endl;
+		std::cout <<  "num[1] : " << num[1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	defaultArray = num;
+	std::cout << "\n\n\033[36m--- Assignment defaultArray = num ---\033[0m\n\n";
+	std::cout << "num.size() : " << num.size() << std::endl;
+	std::cout << "defaultArray.size() : " << defaultArray.size() << std::endl;
+	try
+	{
+		std::cout << "num[0] : " << num[0] << std::endl;
+		std::cout << "defaultArray[0] : " << defaultArray[0] << std::endl;
+		num[0] = 1;
+		std::cout << "\033[32m--- change num[0] value ---\033[0m\n";
+		std::cout <<  "num[0] : " << num[0] << std::endl;
+		std::cout << "defaultArray[0] : " << defaultArray[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Array<int> copy = Array<int>(num);
+	std::cout << "\n\n\033[36m--- Copy by Array<int> copy = Array<int>(num) ---\033[0m\n\n";
+	std::cout << "copy.size() : " << copy.size() << std::endl;
+	std::cout << "copy.size() : " << copy.size() << std::endl;
+	try
+	{
+		std::cout << "num[0] : " << num[0] << std::endl;
+		std::cout << "copy[0] : " << copy[0] << std::endl;
+		copy[0] = 6;
+		std::cout << "\033[32m--- change copy[0] value---\033[0m\n";
+		std::cout << "num[0] : " << num[0] << std::endl;
+		std::cout << "copy[0] : " << copy[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl;
+    }
     return 0;
 }
